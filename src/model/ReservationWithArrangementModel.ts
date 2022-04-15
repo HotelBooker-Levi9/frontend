543f87fa0782f2e8c48cd95bf24d9a943f488c32
GGmaz <kajtes.nikola@gmail.com>
2022-04-15 10:10:03 +0200

    fix: remove comments

--- src/model/ReservationWithArrangementModel.ts
+++ src/model/ReservationWithArrangementModel.ts
@@ -17,25 +17,3 @@ export class ReservationWithArrangementModel implements ReservationWithArrangeme
         this.destinationName = reservation.destinationName;
     }
 }
-
-// export class ReservationFormValues {
-//     id: number = 0;
-//     checkInDate: Date = new Date();
-//     checkOutDate: Date = new Date();
-//     price: number = 0;
-//     guestNumber: number = 0;
-//     isCanceled: boolean = false;
-//     hotelId: number = 0;
-
-//     constructor(reservation? : ReservationFormValues) {
-//         if(reservation) {
-//             this.id = reservation.id;
-//             this.checkInDate = reservation.checkInDate;
-//             this.checkOutDate = reservation.checkOutDate;
-//             this.price = reservation.price;
-//             this.guestNumber = reservation.guestNumber;
-//             this.isCanceled = reservation.isCanceled;
-//             this.hotelId = reservation.hotelId;
-//         }
-//     }
-// }
